002cde3e9d155b05e2bb449c1eee56690c4a4dcc
Praful Mahajan <79085151+Prafulm8o6@users.noreply.github.com>
2023-06-03 11:27:42 +0530

    New Changed In Program.cs File

--- back-end/JwtAuthWebAPI/Program.cs
+++ back-end/JwtAuthWebAPI/Program.cs
@@ -1,5 +1,10 @@
+using JwtAuthCore.Entities;
+using JwtAuthInfrastructure.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,14 +18,24 @@ namespace JwtAuthWebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+				var services = scope.ServiceProvider;
+				try
+				{
+					var _userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+					DbInitializer.InitializeAsync(services, _userManager).Wait();
+				}
+				catch (Exception ex)
+				{
+					var logger = services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "An Error occured");
+				}
+			}
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		public static IWebHost BuildWebHost(string[] args) =>
+			 WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+	}
 }
